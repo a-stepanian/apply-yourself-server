b1ab6e5e611233f774df5f410d20c82601bd72a3
Alex Stepanian <alexstepanian11@gmail.com>
2022-09-22 10:42:03 -0700

    add mongoose package, create user model, refactor user routes to include initial server side validation

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const session = require("express-session");
-
-app.use(session({ secret: "notagoodsecret" }));
 
 // heroku will assign an env var port.
 const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+// set up server
+app.listen(port, () => console.log(`Server started on port: ${port}`));
+
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://client-apply-yourself.netlify.app",
+    ],
+    credentials: true,
+  })
+);
 
+// connect to mongoDB
+mongoose.connect(
+  process.env.ATLAS_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) return console.error(err);
+    console.log("Connected to MongoDB");
+  }
+);
+
+// set up routes
 app.use(require("./routes/applications"));
-app.use(require("./routes/users"));
-
-// get driver connection
-const dbo = require("./db/conn");
-
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer((err) => {
-    if (err) console.error(err);
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+app.use("/auth", require("./routes/userRouter"));
