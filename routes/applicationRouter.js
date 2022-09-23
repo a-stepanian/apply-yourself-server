fcca90a7fa44697963d06fc35008b6418922e4df
Alex Stepanian <alexstepanian11@gmail.com>
2022-09-23 12:01:13 -0700

    update create applicaiton post route so that it also pushes the application into the user's application array

--- routes/applicationRouter.js
+++ routes/applicationRouter.js
@@ -1,12 +1,17 @@
 const router = require("express").Router();
 const Application = require("../models/applicationModel");
+const User = require("../models/userModel");
 const auth = require("../middleware/auth");
 
 //-------------------
 // CREATE NEW APPLICAITON
 //-------------------
 router.post("/", auth, async (req, res) => {
+  console.log("hit route to create new application");
   try {
+    // get mongodb _id from user (added to req object from cookie in auth middleware)
+    const user = req.user;
+    // get remaining properties from req body
     const {
       company,
       position,
@@ -17,8 +22,9 @@ router.post("/", auth, async (req, res) => {
       comments,
       status,
     } = req.body;
-
+    // create new application
     const newApplication = new Application({
+      user,
       company,
       position,
       website,
@@ -28,11 +34,16 @@ router.post("/", auth, async (req, res) => {
       comments,
       status,
     });
-
+    // save to db
     const savedApplication = await newApplication.save();
+    // Find user in db
+    const foundUser = await User.findById(user);
+    // Push new app into the user's applications array.
+    foundUser.applications.push(savedApplication);
+    await foundUser.save();
 
     res.json(savedApplication);
-  } catch (error) {
+  } catch (err) {
     console.error(err);
     res.status(500).send();
   }
@@ -41,5 +52,16 @@ router.post("/", auth, async (req, res) => {
 //-------------------
 // GET ALL APPLICATIONS
 //-------------------
+
+router.get("/", auth, async (req, res) => {
+  const id = req.user;
+  console.log("id:", id);
+  try {
+    const existingUser = await User.find({ id });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send();
+  }
+});
 
 module.exports = router;
