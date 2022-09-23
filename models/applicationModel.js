6753621f99818f70a06b94ce43ed894b3b018a40
Alex Stepanian <alexstepanian11@gmail.com>
2022-09-23 11:58:38 -0700

    update user and application schemas with one-to-many relationship

--- models/applicationModel.js
+++ models/applicationModel.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const applicationSchema = new mongoose.Schema({
+const applicationSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
   company: { type: String, required: true },
   position: { type: String, required: true },
   website: { type: String, required: true },
   location: { type: String, required: true },
   applied: { type: String, required: true },
-  response: { type: String, required: true },
+  response: { type: String },
   comments: { type: String, required: true },
   status: { type: String, required: true },
 });
--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   passwordHash: { type: String, required: true },
-  applications: { type: Array, required: true },
+  applications: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Application",
+    },
+  ],
 });
 
 const User = mongoose.model("user", userSchema);
