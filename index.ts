8778df49b28fa98faa8933f8277f8ec8b16ea73d
Alex Stepanian <alexstepanian11@gmail.com>
2024-08-01 14:19:53 -0700

    update payload limit

--- index.ts
+++ index.ts
@@ -21,7 +21,8 @@ mongoose
 
 // Initialize server
 const app: Express = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
